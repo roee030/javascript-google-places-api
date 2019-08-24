a6e68cca16244eaac0ab359c565365970ff4e9e7
roee030 <roee.angel93@gmail.com>
2019-08-24 22:58:28 +0300

    Update app.js
    finish with images,name,address.
    I nees to calculate the distance from user to the restaurnt location.
    after that to show in the web screen.

--- app.js
+++ app.js
@@ -9,25 +9,42 @@ function apiGetAll () {
 var data_from_api = [];
 //our query with api-key included Gosh-Dan Location
 var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.0333332,34.7666636&radius=1500&type=restaurant&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A&fields=opening_hours,photos,rating'
-
+while(next_page)
+{
 fetch(url, {
     method: 'get'
 }).then(response =>response.json()).then(data => {
+   
+    
     for(var i = 0 ; i < data.results.length ; i++)
     {
         //address - check
         var address_of_restaurant = data.results[i].vicinity;
-      
-        //name
+        //image - check
+        if(data.results[i].photos)
+        { image_of_restaurant = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference='+data.results[i].photos[0].photo_reference+'&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A'}
+        else { image_of_restaurant = undefined}
+        //name - check
         var name_of_restaurant = data.results[i].name;
-      
-        console.log(address_of_restaurant);
+
+        //add to an object
+        //data_from_api.push(name_of_restaurant,image_of_restaurant);
+        //console.log(i + name_of_restaurant + address_of_restaurant  )
+
+        //distance user from the restaurant
+        
     }
+    
 
-   //console.log(data_from_api);
-
+    
 } )
-
+if(data.next_page_token){
+    url='https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.0333332,34.7666636&radius=1500&type=restaurant&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A&fields=opening_hours,photos,rating&pagetoken='+data.next_page_token;
+}
+else{
+    next_page=false;
 }
 
+}
+}
 apiGetAll();
