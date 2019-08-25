c82e45be90cd5bd755355f3902105d21cdc71816
roee030 <roee.angel93@gmail.com>
2019-08-25 04:52:07 +0300

    running over all token
    get info from the api

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 //global var
 var data_from_api = [];
-var next_token;
 var address_of_restaurant;
 var image_of_restaurant;
-var next_page = true;
+var next_page = [];
+var next_token;
+var toContinue = false;
+var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.0333332,34.7666636&radius=1500&type=restaurant&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A&fields=opening_hours,photos,rating.json'
+
 //gets all information needed
-function apiGetAll () {
+
 var data_from_api = [];
 //our query with api-key included Gosh-Dan Location
-var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.0333332,34.7666636&radius=1500&type=restaurant&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A&fields=opening_hours,photos,rating'
-while(next_page)
+function getdatafirsttry(url)
 {
-fetch(url, {
-    method: 'get'
-}).then(response =>response.json()).then(data => {
-   
+    fetch(url,{method: 'get'})
+    .then(response =>
+        response.json())
+    .then(data => {
+    next_token = data.next_page_token;
     
+    if(next_token !== undefined){
+    url='https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.0333332,34.7666636&radius=1500&type=restaurant&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A&fields=opening_hours,photos,rating.json&pagetoken='+next_token;
+    toContinue = true;
+    }
     for(var i = 0 ; i < data.results.length ; i++)
     {
         //address - check
         var address_of_restaurant = data.results[i].vicinity;
-        //image - check
+      /*  //image - check
         if(data.results[i].photos)
         { image_of_restaurant = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference='+data.results[i].photos[0].photo_reference+'&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A'}
         else { image_of_restaurant = undefined}
@@ -31,20 +38,24 @@ fetch(url, {
         //data_from_api.push(name_of_restaurant,image_of_restaurant);
         //console.log(i + name_of_restaurant + address_of_restaurant  )
 
-        //distance user from the restaurant
-        
+        //distance user from the restaurant*/
+        console.log(address_of_restaurant)
     }
     
+} ).then( (success, err) => {
+    if (err) { console.error(err); }
+    if(toContinue){
+    alltogheter(url);
+    }
+})
 
-    
-} )
-if(data.next_page_token){
-    url='https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.0333332,34.7666636&radius=1500&type=restaurant&key=AIzaSyCbaehPkHsIMdyfCjDRdfMlZHSVrl12E_A&fields=opening_hours,photos,rating&pagetoken='+data.next_page_token;
-}
-else{
-    next_page=false;
-}
 
 }
+
+getdatafirsttry(url);
+function alltogheter(url)
+{
+   
+    getdatafirsttry(url);
+
 }
-apiGetAll();
